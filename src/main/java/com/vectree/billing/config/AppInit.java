c0a2ba2d03656cc66b13567b5b68e94d0a67b3e8
vakhnenzon <gmail@vakhnenko.com>
2018-08-02 21:11:05 +0300

    init

---
+++ src/main/java/com/vectree/billing/config/AppInit.java
@@ -0,0 +1,33 @@
+package com.vectree.billing.config;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+/**
+ * class AppInit WebApplicationInitializer Spring MVC.
+ * @version 1.0
+ */
+public class AppInit implements WebApplicationInitializer {
+    private static final String DISPATCHER_SERVLET_NAME = "billing";
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+
+        ctx.register(WebConfig.class);
+        //servletContext.addListener(new ContextLoaderListener(ctx));
+        ctx.setServletContext(servletContext);
+
+        ServletRegistration.Dynamic servlet = servletContext.addServlet(DISPATCHER_SERVLET_NAME,
+                new DispatcherServlet(ctx));
+        servlet.addMapping("/");
+        servlet.setLoadOnStartup(1);
+    }
+
+}
