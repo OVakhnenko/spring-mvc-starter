159566e8cc9dad7502c5e7490caedbe4aa5cd8e2
vakhnenzon <gmail@vakhnenko.com>
2018-08-03 11:58:36 +0300

    config starter commit 2

--- src/main/java/com/vectree/billing/config/AppInit.java
+++ src/main/java/com/vectree/billing/config/AppInit.java
@@ -1,6 +1,7 @@
 package com.vectree.billing.config;
 
 import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
--- src/main/java/com/vectree/billing/config/RootConfig.java
+++ src/main/java/com/vectree/billing/config/RootConfig.java
@@ -1,10 +1,13 @@
 package com.vectree.billing.config;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
 /**
  * class RootConfig Spring MVC.
  * @version 1.0
  */
-//@Configuration
-//@ComponentScan({"com.vectree.billing.*"})
+@Configuration
+@ComponentScan({"com.vectree.billing.*"})
 public class RootConfig {
 }
